2b25d32963d8827e0474afe79820c23066c60e66
Brennan Roel <brennanroel@gmail.com>
2021-02-27 01:36:53 -0600

    index file setup for redux

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux'
+
 import './index.css';
 import Header from "./Header";
 import App from './App';
 import Footer from './Footer';
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
-  <div>
+  <Provider store={store}>
     <Header />
     <App />
     <Footer />
-  </div>,
+  </Provider>,
   document.getElementById('root')
 );
