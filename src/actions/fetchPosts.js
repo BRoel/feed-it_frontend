864821d259b502491dccdab9bfb54d3fb4afbf4d
Brennan Roel <brennanroel@gmail.com>
2021-02-28 14:43:16 -0600

    finished fetchPosts

--- src/actions/fetchPosts.js
+++ src/actions/fetchPosts.js
@@ -1,6 +1,10 @@
 export function fetchPosts(){
-    return (dispatch)
-    fetch('http://localhost:3000/api/v1/posts')
-    .then(response => response.json())
-    .then(data => console.log(data))
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/posts')
+        .then(response => response.json())
+        .then(posts => dispatch({
+            type: 'FETCH_POSTS', //neccesary key for dispatch
+            payload: posts //fetching array from backend to use to update store
+        }))
+    }
 }
