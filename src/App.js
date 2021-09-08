86dcabf06b4be632de2bdddc0488c4ab7155afff
Brennan Roel <brennanroel@gmail.com>
2021-09-08 12:51:37 -0500

    front end of Auth0 login working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
+import LoginButton from './components/Auth0/LoginButton';
+import LogoutButton from './components/Auth0/LogoutButton';
 import NavBar from './components/NavBar';
 import ScrollToTop from './components/ScrollToTop';
 import PostsContainer from './containers/PostsContainer'
 import Footer from './Footer';
+import { useAuth0 } from '@auth0/auth0-react';
 import './index.css';
 
-class App extends React.Component {
+function App() {
 
-  render() {
-    return (
-      <div>
-        <ScrollToTop/>
-        <NavBar/>
-        <div id='wrap'>
-          <div id="main">
-            <PostsContainer/>
-          </div>
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    <div>
+      <ScrollToTop/>
+      <NavBar/>
+      <div id='wrap'>
+        <div id="main">
+          <h3>User is { isAuthenticated ? "Logged In" : "Logged Out"}</h3>
+          <LoginButton/>
+          <LogoutButton/>
+          <PostsContainer/>
         </div>
-        <Footer/>
       </div>
-    );
-  }
+      <Footer/>
+    </div>
+  );
 }
 
 export default (App);
