f7a3ef13db5be9cd2af0578a393a092d67c3884a
Brennan Roel <brennanroel@gmail.com>
2021-10-04 13:23:18 -0500

    added auth to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function NavBar() {
 
@@ -9,6 +10,9 @@ function NavBar() {
     const handleClick = () => setClick(!click);
 
     const closeMobileMenu = () => setClick(false)
+    const {
+        isAuthenticated,
+    } = useAuth0();
 
     return (
         <>
@@ -31,16 +35,28 @@ function NavBar() {
                                 All Posts
                             </Link>
                         </li>
-                        <li className='nav-item'>
-                            <Link to='/posts/new' className='nav-links' onClick={closeMobileMenu}>
-                                Add Post
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
+                        {
+                            isAuthenticated ? 
+                            <li className='nav-item'>
+                                <Link to='/posts/new' className='nav-links' onClick={closeMobileMenu}>
+                                    Add Post
+                                </Link>
+                            </li> 
+                            : 
+                            <li className='nav-item'>
+                                <Link to='/registration' className='nav-links' onClick={closeMobileMenu}>
+                                    Login/Signup
+                                </Link>
+                            </li> 
+                        }
+                        { 
+                            isAuthenticated ? 
+                            <li className='nav-item'>
                             <Link to='/profile' className='nav-links' onClick={closeMobileMenu}>
-                                <i className='far fa-user-circle'></i>
-                            </Link>
-                        </li> 
+                            <i className='far fa-user-circle'></i>
+                            </Link> 
+                            </li> : null 
+                        }
                     </ul>
                 </div>
             </nav>
