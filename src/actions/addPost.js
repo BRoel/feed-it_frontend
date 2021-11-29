59540659da441596d512a6e8aa79833b552320ef
Brennan Roel <brennanroel@gmail.com>
2021-11-29 11:59:34 -0600

    added token passed to api

--- src/actions/addPost.js
+++ src/actions/addPost.js
@@ -1,19 +1,28 @@
-export const addPost = (data) => {
+import { DirectUpload } from 'activestorage';
+
+export const addPost = (data, token) => {
+
+  const uploadFile = (file, post) => {
+    const upload = new DirectUpload(file, 'http://localhost:3000/rails/active_storage/direct_uploads')
+  }
 
   return (dispatch) => {
-    
+
     fetch('http://localhost:3000/api/v1/posts', {
       headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        'Accept': 'application/json',
+        'Authorization': token
         //extract header for auth0
       },
       method: 'POST',  // POST to API adds new post
       body: JSON.stringify(data)
     })
     .then(response => response.json())
+    // .then(data => uploadFile(this.state.photo, data))
     .then(post => dispatch({type: 'ADD_POST', payload: post}))
     //dispatched new POST to Postreducer
+
   }
   
 }
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -10,41 +10,56 @@ class PostForm extends React.Component {
         this.state = this.getInitialState()
     }
 
+    componentDidMount() {
+        const { getAccessTokenSilently } = this.props.auth0;
+        getAccessTokenSilently().then((value) => this.setState({token: value}))
+    }
+
     getInitialState(){
         return(
             { 
                 title: '',
                 image: '',
                 body: '',
+                photo: ''
             }
         );
     }
 
     handleChange = (e) => { //update state
-
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-        
+        if (e.target.name === 'photo') {
+            this.setState({
+                [e.target.name]: e.target.files[0]
+            })
+        } else {
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }
     }
 
     handleSubmit = (e) => { //on form submit state sent to addPost action
         e.preventDefault()
         const { user, isAuthenticated, getAccessTokenSilently } = this.props.auth0;
         // in order to get access token I must call getAccessTokenSilently.
-        // also the example I sent is using react hooks so I cannot do it the way shown unless I switch this to a functional component setup.  The hook setup is commented out above.
-        // may need to conver to a componentDidMount setup so on mount I can assess if they are authenticated and grab their access token.
+        // also the example I sent is using react hooks so I cannot do it the way shown unless I switch this to a functional component setup.
+        // may need to convert to a componentDidMount setup so on mount I can assess if they are authenticated and grab their access token.
 
         const postWithUser = Object.assign({}, this.state)
         postWithUser.user_id = user.sub
         postWithUser.nickname = user.nickname
-        this.props.addPost(postWithUser)
+        // let token = await getAccessTokenSilently();
+        this.props.addPost(postWithUser, this.state.token)
 
         this.setState(this.getInitialState())
         this.props.history.push('/posts')
     }
 
     render() {
+
+        if(!this.state.token) {
+            return <div> </div>
+        }
         return(
             <div>
                 <form className='post' onSubmit={this.handleSubmit}>
@@ -53,6 +68,7 @@ class PostForm extends React.Component {
                     <br></br>
                     <label>Post Photo URL:</label>
                     <input type="text" name="image" value={this.state.image} onChange={this.handleChange} />
+                    <input type="file" name="photo" value={this.state.photo} onChange={this.handleChange} />
                     <br></br>
                     <label>Post Content:<strong className='require'> required*</strong></label>
                     <textarea type='text' placeholder='Content' value={this.state.body} name="body" onChange={this.handleChange} required/>
